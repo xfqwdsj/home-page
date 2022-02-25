f1c43d4a67bf0a8409cc000cf2047a8ce3dd2070
LTFan <xfqwdsj@qq.com>
2022-02-25 16:25:51 +0800

    .

--- components/page/head.tsx
+++ components/page/head.tsx
@@ -16,55 +16,48 @@ export interface HeadProps {
   topBarTitle: string;
 }
 
-const AppHead = ({ pageTitle, pageDescription, topBarTitle }: HeadProps) => {
-  const theme = useTheme();
-  theme.palette.primary.main =
-    theme.palette.mode === 'light' ? '#ffffff1a' : '#0000001a';
+const AppHead = ({ pageTitle, pageDescription, topBarTitle }: HeadProps) => (
+  <>
+    <Head>
+      <title>{pageTitle}</title>
+      <meta name="description" content={pageDescription} />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-  return (
-    <>
-      <Head>
-        <title>{pageTitle}</title>
-        <meta name="description" content={pageDescription} />
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <ThemeProvider
-        theme={(theme: Theme) =>
-          createTheme({
-            ...theme,
-            palette: {
-              ...theme.palette,
-              primary: {
-                main:
-                  theme.palette.mode === 'light' ? '#ffffff1a' : '#0000001a',
-              },
+    <ThemeProvider
+      theme={(theme: Theme) =>
+        createTheme({
+          ...theme,
+          palette: {
+            ...theme.palette,
+            primary: {
+              main: theme.palette.mode === 'light' ? '#ffffff1a' : '#0000001a',
             },
-          })
-        }
+          },
+        })
+      }
+    >
+      <AppBar
+        position="fixed"
+        sx={{
+          boxShadow: 0,
+          borderBottom: 1,
+          borderColor: (theme) =>
+            theme.palette.mode === 'light' ? grey[300] : grey[900],
+          backdropFilter: 'blur(10px)',
+        }}
+        enableColorOnDark
       >
-        <AppBar
-          position="fixed"
-          sx={{
-            boxShadow: 0,
-            borderBottom: 1,
-            borderColor: (theme) =>
-              theme.palette.mode === 'light' ? grey[300] : grey[900],
-            backdropFilter: 'blur(10px)',
-          }}
-          enableColorOnDark
-        >
-          <Toolbar>
-            <Typography variant="h6" component="div">
-              {topBarTitle}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Toolbar />
-      </ThemeProvider>
-    </>
-  );
-};
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            {topBarTitle}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+    </ThemeProvider>
+  </>
+);
 
 export default AppHead;
