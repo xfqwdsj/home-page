cbc1fd401e5f72cca056606a2756c6c20dd8cc88
LTFan <xfqwdsj@qq.com>
2022-03-02 01:43:18 +0800

    。

--- components/gobang/board_components.tsx
+++ components/gobang/board_components.tsx
@@ -1,49 +1,44 @@
 import {styled} from "@mui/material";
 import {SVGProps} from "react";
+import {Status} from "./gobang";
 
-type PointProps = ({
-    type: "topLeft" | "topRight" | "bottomLeft" | "bottomRight" | "all" | "left" | "top" | "right" | "bottom"
-        | "noLeft" | "noTop" | "noRight" | "noBottom" | "vertical" | "horizontal"
-    pointType: "normal" | "main" | "black" | "white"
-} | { type: "none" }) & { size: number }
+export type Normal = "normal"
+export type Main = "main"
+export type PointTypes = Normal | Main
 
-export const Point = (props: PointProps) => {
-    if (props.size < 40) return <></>;
-    const {size} = props;
+type PointProps = {
+    size: number
+    left: boolean
+    top: boolean
+    right: boolean
+    bottom: boolean
+    pointType: PointTypes | Status
+} | {
+    size: number
+}
+
+export const Point = ({size, left, top, right, bottom, pointType}: PointProps) => {
+    if (size < 40) return <></>;
     const defaultSvg = (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => <svg
         xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${size} ${size}`} {...props}/>;
     const Svg = styled(defaultSvg)({
         width: size,
         height: size,
     });
-    if (props.type === "none") return <Svg/>;
-    const {type, pointType} = props;
     let back: JSX.Element;
     let path = "";
     let front = <path/>;
-    const chess = `M ${size / 4} ${size / 4} a ${size / 4} ${size / 4} 0 0 0 ${size / 2} 0 a ${size / 4} ${size / 4} 0 0 0 -${size / 2} 0 Z`;
-    if (type === "topRight" || type === "bottomRight" || type === "all" || type === "left" || type === "noTop"
-        || type === "noRight" || type === "noBottom" || type === "horizontal") {
-        path += `M 0 ${size / 2 - 0.5} h ${size / 2} v 1 h -${size / 2} Z`;
-    }
-    if (type === "bottomLeft" || type === "bottomRight" || type === "all" || type === "top" || type === "noLeft"
-        || type === "noRight" || type === "noBottom" || type === "vertical") {
-        path += `M ${size / 2 - 0.5} 0 v ${size / 2} h 1 v -${size / 2} Z`;
-    }
-    if (type === "topLeft" || type === "bottomLeft" || type === "all" || type === "right" || type === "noLeft"
-        || type === "noTop" || type === "noBottom" || type === "horizontal") {
-        path += `M ${size / 2} ${size / 2 - 0.5} h ${size / 2} v 1 h -${size / 2} Z`;
-    }
-    if (type === "topLeft" || type === "topRight" || type === "all" || type === "bottom" || type === "noLeft"
-        || type === "noTop" || type === "noRight" || type === "vertical") {
-        path += `M ${size / 2 - 0.5} ${size / 2} v ${size / 2} h 1 v -${size / 2} Z`;
-    }
+    const chess = `M ${size / 6} ${size / 2} a ${size / 6} ${size / 6} 0 0 0 ${size / 3} 0 a ${size / 6} ${size / 6} 0 0 0 -${size / 3} 0 Z`;
+    if (left) path += `M 0 ${size / 2 - 0.5} h ${size / 2} v 1 h -${size / 2} Z`;
+    if (top) path += `M ${size / 2 - 0.5} 0 v ${size / 2} h 1 v -${size / 2} Z`;
+    if (right) path += `M ${size / 2} ${size / 2 - 0.5} h ${size / 2} v 1 h -${size / 2} Z`;
+    if (bottom) path += `M ${size / 2 - 0.5} ${size / 2} v ${size / 2} h 1 v -${size / 2} Z`;
     switch (pointType) {
         case "normal":
-            path += `M ${size / 2 - 2.5} ${size / 2} a 2.5 2.5 0 0 0 2.5 0 a 2.5 2.5 0 0 0 -2.5 0 Z`;
+            path += `M ${size / 2 - 2.5} ${size / 2} a 2.5 2.5 0 0 0 5 0 a 2.5 2.5 0 0 0 -5 0 Z`;
             break;
         case "main":
-            path += `M ${size / 2 - 5} ${size / 2} a 5 5 0 0 0 5 0 a 5 5 0 0 0 -5 0 Z`;
+            path += `M ${size / 2 - 5} ${size / 2} a 5 5 0 0 0 10 0 a 5 5 0 0 0 -10 0 Z`;
             break;
         case "black":
             front = <path d={chess} fill="#000" stroke="#fff"/>;
--- components/gobang/gobang.tsx
+++ components/gobang/gobang.tsx
@@ -2,19 +2,35 @@ import {useState} from "react";
 import {Stack} from "@mui/material";
 import {Point} from "./board_components";
 
+export type Black = "black"
+export type White = "white"
+export type Status = Black | White
+
 type GobangProps = {
-    board: (null | undefined | 0 | 1)[][]
-    onBoardStateChange: (board: (null | undefined | 0 | 1)[][]) => void
+    board: (null | undefined | Status)[][]
+    onBoardStateChange: (board: (null | undefined | Status)[][]) => void
 }
 
 const Gobang = ({board, onBoardStateChange}: GobangProps) => {
-    const [current, setCurrent] = useState<0 | 1>(0);
+    const [current, setCurrent] = useState<Status>(black);
 
     return (<>
-        <Point type="topLeft" size={50} pointType="normal"/>
-        <Point type="noTop" size={50} pointType="main"/>
-        <Point type="right" size={50} pointType="black"/>
-        <Point type="horizontal" size={100} pointType="white"/>
+        <Stack overflow="scroll">
+            {board.map((columns, rowIndex) => {
+                return <Stack direction="row" key={rowIndex}>
+                    {columns.map((point, columnIndex) => {
+                        if (point === undefined) return <Point size={100}/>
+                        const type = {
+                            left: columns[columnIndex - 1] !== undefined,
+                            top: board[rowIndex - 1][columnIndex] !== undefined,
+                            right: columns[columnIndex + 1] !== undefined,
+                            bottom: board[rowIndex + 1][columnIndex] !== undefined
+                        };
+                        return <Point size={100} {...type} pointType={column ? column : "normal"}/>
+                    })}
+                </Stack>;
+            })}
+        </Stack>
     </>);
 };
 
