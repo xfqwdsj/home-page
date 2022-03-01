c440cbbc6b98d13cca352538327af0ed2fa44fab
LTFan <xfqwdsj@qq.com>
2022-03-02 02:23:53 +0800

    。

--- components/gobang/gobang.tsx
+++ components/gobang/gobang.tsx
@@ -18,14 +18,14 @@ const Gobang = ({board, onBoardStateChange}: GobangProps) => {
             {board.map((columns, rowIndex) => {
                 return <Stack direction="row" key={rowIndex}>
                     {columns.map((point, columnIndex) => {
-                        if (point === undefined) return <Point size={100} key={columnIndex}/>
+                        if (point === undefined) return <Point size={100} key={columnIndex}/>;
                         const type = {
                             left: columns[columnIndex - 1] !== undefined,
-                            top: board[rowIndex - 1][columnIndex] !== undefined,
+                            top: board[rowIndex - 1] && board[rowIndex - 1][columnIndex] !== undefined,
                             right: columns[columnIndex + 1] !== undefined,
-                            bottom: board[rowIndex + 1][columnIndex] !== undefined
+                            bottom: board[rowIndex + 1] && board[rowIndex + 1][columnIndex] !== undefined,
                         };
-                        return <Point size={100} {...type} pointType={point ? point : "normal"} key={columnIndex}/>
+                        return <Point size={100} {...type} pointType={point ? point : "normal"} key={columnIndex}/>;
                     })}
                 </Stack>;
             })}
