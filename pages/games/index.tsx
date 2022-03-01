41e95de61c413297e2fc9a52a4534b3883138b9b
LTFan <xfqwdsj@qq.com>
2022-03-01 12:35:15 +0800

    .

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,10 +1,12 @@
 import { NextPage } from 'next';
+import { GobangIcon } from '../../components/icons';
+import { MasonryCards } from '../../components/masonryCards';
 import { HeadProps } from '../../components/page/head';
 
 const head: HeadProps = {
-  pageTitle: 'Clash | LTFan',
-  pageDescription: "LTFan's Clash configs console.",
-  topBarTitle: 'Clash',
+  pageTitle: '游戏 | LTFan',
+  pageDescription: 'Some games.',
+  topBarTitle: '游戏',
 };
 
 export const getStaticProps = () => {
@@ -15,10 +17,21 @@ export const getStaticProps = () => {
   };
 };
 
+const cards = [
+  {
+    name: '五子棋',
+    icon: GobangIcon,
+    description: '最多两人的对战游戏',
+    href: 'gobang/',
+  },
+];
+
 const Games: NextPage = () => {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <MasonryCards cards={cards} />
+    </>
+  );
 };
 
 export default Games;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,7 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Grid,
-  SvgIconTypeMap,
-  Typography,
-} from '@mui/material';
-import { OverridableComponent } from '@mui/material/OverridableComponent';
 import type { NextPage } from 'next';
-import { UrlObject } from 'url';
-import { ClashIcon } from '../components/icons/clash';
+import { ClashIcon } from '../components/icons';
 import GamesIcon from '@mui/icons-material/Games';
 import BugReportIcon from '@mui/icons-material/BugReport';
-import { NextLinkComposed } from '../components/link';
 import { HeadProps } from '../components/page/head';
 import { MasonryCards } from '../components/masonryCards';
 
@@ -31,21 +19,10 @@ export const getStaticProps = () => {
   };
 };
 
-interface HomeApp {
-  name: string;
-  icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> & {
-    muiName: string;
-  };
-  viewBox?: string;
-  description: string;
-  href: string | UrlObject;
-}
-
 const cards = [
   {
     name: 'Clash',
     icon: ClashIcon,
-    viewBox: '0 0 512 512',
     description: 'Clash 配置多用户托管服务',
     href: 'clash/',
   },
@@ -54,18 +31,6 @@ const cards = [
     icon: GamesIcon,
     description: '适合网页游玩的小游戏',
     href: 'games/',
-  },
-  {
-    name: '测试',
-    icon: BugReportIcon,
-    description: '测试页面，你永远想不到我会在里面放些什么',
-    href: 'test/',
-  },
-  {
-    name: '测试',
-    icon: BugReportIcon,
-    description: '测试页面，你永远想不到我会在里面放些什么',
-    href: 'test/',
   },
   {
     name: '测试',
