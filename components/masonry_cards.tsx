9e71311601d620961e65bf9953d549a41663a670
LTFan <xfqwdsj@qq.com>
2022-03-02 09:00:13 +0800

    。

--- components/masonry_cards.tsx
+++ components/masonry_cards.tsx
@@ -1,5 +1,4 @@
-import {Masonry} from "@mui/lab";
-import {Button, Card, CardActions, CardContent, SvgIconTypeMap, Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Grid, SvgIconTypeMap, Typography} from "@mui/material";
 import {OverridableComponent} from "@mui/material/OverridableComponent";
 import {UrlObject} from "url";
 import {NextLinkComposed} from "./link";
@@ -15,20 +14,21 @@ export type CardInfo = {
 };
 
 export const MasonryCards = (props: { cards: CardInfo[] }) => (
-    <Masonry columns={{xs: 2, sm: 3, md: 4}} spacing={2}>
+    <Grid container columns={60} spacing={2} sx={{alignItems: "stretch"}}>
         {props.cards.map((card) => (
-            <Card key={card.toString()} sx={{mx: "auto"}}>
-                <CardContent>
-                    <card.icon sx={{width: 1, height: 80}} viewBox={card.viewBox}/>
-                    <Typography variant="h5">{card.name}</Typography>
-                    <Typography color="text.secondary">{card.description}</Typography>
-                </CardContent>
-                <CardActions>
-                    <Button size="small" component={NextLinkComposed} to={card.href}>
-                        去看看
-                    </Button>
-                </CardActions>
-            </Card>
+            <Grid item xs={30} sm={20} md={15} xl={12} key={card.toString()}>
+                <Card sx={{width: 1}}>
+                    <CardContent>
+                        <card.icon sx={{width: 1, height: 80}} viewBox={card.viewBox}/>
+                        <Typography variant="h5">{card.name}</Typography>
+                        <Typography color="text.secondary">{card.description}</Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" component={NextLinkComposed} to={card.href}>
+                            去看看
+                        </Button>
+                    </CardActions>
+                </Card></Grid>
         ))}
-    </Masonry>
+    </Grid>
 );
