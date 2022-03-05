97255fec5cb96d9627e34fd3d8185cea4b378779
LTFan <xfqwdsj@qq.com>
2022-03-05 20:58:51 +0800

    refactor: 赢家采用的来源

--- pages/games/gobang/nearby.tsx
+++ pages/games/gobang/nearby.tsx
@@ -40,16 +40,16 @@ const doOnPointClick: Reducer<
         tmp[x].array[y].point = nextPlayer.current;
         const winner = getWinner(board, x, y);
         if (winner) {
+            nextPlayer.current = null;
             const onCancel = () => dialog.setOpen(false);
             header.setTopBarTitle(
-                `赢家：${nextPlayer.current} | ${head.topBarTitle}`
+                `赢家：${winner} | ${head.topBarTitle}`
             );
             dialog.setTitle("赢了！");
             dialog.setContent(<>{`恭喜：${winner}`}</>);
             dialog.setActions(<Button onClick={onCancel}>确定</Button>);
             dialog.setOnCancel(() => onCancel);
             dialog.setOpen(true);
-            nextPlayer.current = null;
         } else {
             nextPlayer.current =
                 nextPlayer.current === "black" ? "white" : "black";
