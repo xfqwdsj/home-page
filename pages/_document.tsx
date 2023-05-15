1e97fff635321c0efc74694326c0d8fa1907b857
xfqwdsj <xfqwdsj@qq.com>
2023-05-15 14:04:02 +0800

    chore: 更新依赖

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,15 @@
-import Document, {Head, Html, Main, NextScript} from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
-    render(): JSX.Element {
-        return (
-            <Html lang="zh-CN">
-                <Head/>
-                <body>
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
-        );
-    }
+  render(): JSX.Element {
+    return (
+      <Html lang="zh-CN">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,49 @@
-import type {NextPage} from "next";
-import {ClashIcon} from "../components/icons";
+import type { NextPage } from "next";
+import { ClashIcon } from "../components/icons";
 import GamesIcon from "@mui/icons-material/Games";
 import BugReportIcon from "@mui/icons-material/BugReport";
-import {HeadProps} from "../components/head";
-import {CardInfo, CardsGrid} from "../components/cards_grid";
+import { HeadProps } from "../components/head";
+import { CardInfo, CardsGrid } from "../components/cards_grid";
 
 const head: HeadProps = {
-    pageTitle: "LTFan",
-    pageDescription: "LTFan's home page",
-    topBarTitle: "LTFan",
+  pageTitle: "LTFan",
+  pageDescription: "LTFan's home page",
+  topBarTitle: "LTFan",
 };
 
 export const getStaticProps = () => ({
-    props: {
-        head,
-    },
+  props: {
+    head,
+  },
 });
 
 const cards: CardInfo[] = [
-    {
-        name: "Clash",
-        icon: ClashIcon,
-        description: "Clash 配置多用户托管服务",
-        href: "/clash/",
-    },
-    {
-        name: "游戏",
-        icon: GamesIcon,
-        description: "适合网页游玩的小游戏",
-        href: "/games/",
-    },
-    {
-        name: "测试",
-        icon: BugReportIcon,
-        description: "测试页面，你永远想不到我会在里面放些什么",
-        href: "/test/",
-    },
+  {
+    name: "Clash",
+    icon: ClashIcon,
+    description: "Clash 配置多用户托管服务",
+    href: "/clash/",
+  },
+  {
+    name: "游戏",
+    icon: GamesIcon,
+    description: "适合网页游玩的小游戏",
+    href: "/games/",
+  },
+  {
+    name: "测试",
+    icon: BugReportIcon,
+    description: "测试页面，你永远想不到我会在里面放些什么",
+    href: "/test/",
+  },
 ];
 
 const Home: NextPage = () => {
-    return (
-        <>
-            <CardsGrid cards={cards}/>
-        </>
-    );
+  return (
+    <>
+      <CardsGrid cards={cards} />
+    </>
+  );
 };
 
 export default Home;
