e8abfd7902445bb4bf830e7fe3506d6f03267edc
LTFan <xfqwdsj@qq.com>
2026-02-17 22:17:44 +0800

    fix: JSX reference (1)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { JSX } from "react";
 
 export default class MyDocument extends Document {
   render(): JSX.Element {
